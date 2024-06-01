ca464f1b190744a4ca78a681693801695e19d9a6
nacholozano <nacholozanogui@gmail.com>
2024-06-01 16:31:09 +0200

    test: fix PodcastsCard test

--- src/ui/components/PodcastCard/PodcastsCard.test.tsx
+++ src/ui/components/PodcastCard/PodcastsCard.test.tsx
@@ -1,18 +1,35 @@
+import { ReactNode } from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import PodcastCard from './PodcastCard'
 
+jest.mock('react-router-dom', () => ({
+  Link: ({ children }: { children: ReactNode }) => <>{children}</>,
+}))
+
 describe('PodcastsCard component', () => {
   test('Show title', async () => {
     render(
-      <PodcastCard title="title-test" img="" alt="" author=""></PodcastCard>
+      <PodcastCard
+        title="title-test"
+        img=""
+        alt=""
+        author=""
+        path=""
+      ></PodcastCard>
     )
     expect(screen.getByTestId('title')).toHaveTextContent('title-test')
   })
 
   test('Show author', async () => {
     render(
-      <PodcastCard title="" img="" alt="" author="author-test"></PodcastCard>
+      <PodcastCard
+        title=""
+        img=""
+        alt=""
+        author="author-test"
+        path=""
+      ></PodcastCard>
     )
     expect(screen.getByTestId('author')).toHaveTextContent(
       'Author: author-test'
@@ -26,6 +43,7 @@ describe('PodcastsCard component', () => {
         img="image.png"
         alt="title-test"
         author=""
+        path=""
       ></PodcastCard>
     )
     expect(screen.getByAltText('title-test')).toHaveAttribute(
