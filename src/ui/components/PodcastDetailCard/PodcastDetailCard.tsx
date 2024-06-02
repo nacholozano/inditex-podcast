a38e33d98d5e4f7b84c6e4bcea689816b4079c0c
nacholozano <nacholozanogui@gmail.com>
2024-06-02 08:42:55 +0200

    feat: add link to podcast detail in podcast detail card

--- src/ui/components/PodcastDetailCard/PodcastDetailCard.tsx
+++ src/ui/components/PodcastDetailCard/PodcastDetailCard.tsx
@@ -1,19 +1,34 @@
+import { Link, useParams } from 'react-router-dom'
 import Separator from 'ui/components/Separator/Separator'
 import Card from 'ui/components/Card/Card'
 import { Props } from './types'
 import styles from './styles.module.css'
 
 const PodcastDetailCard = ({ img, alt, title, author, description }: Props) => {
+  const { podcastId } = useParams()
+  const pathTopodcast = `/podcast/${podcastId}`
+
   return (
     <Card>
       <div className={styles.container}>
         <div className={styles.imgContainer}>
-          <img src={img} alt={alt} className={styles.img} />
+          <Link to={pathTopodcast}>
+            <img src={img} alt={alt} className={styles.img} />
+          </Link>
         </div>
         <Separator />
         <div className={styles.infoContainer}>
-          <div className={styles.title}>{title}</div>
-          <div className={styles.author}>by {author}</div>
+          <div className={styles.title}>
+            <Link to={pathTopodcast} className={styles.link}>
+              {title}
+            </Link>
+          </div>
+          <div className={styles.author}>
+            by{' '}
+            <Link to={pathTopodcast} className={styles.link}>
+              {author}
+            </Link>
+          </div>
         </div>
         <Separator />
         <div className={styles.descContainer}>
